62ebec32a59580f056a6293134aa6c46196f4ae8
Roman Volodin <rozarioagro@gmail.com>
2022-12-13 22:48:30 +0300

    tests done

--- go.mod
+++ go.mod
@@ -2,7 +2,12 @@ module github.com/RomanAVolodin/go-url-shortener
 
 go 1.19
 
+require github.com/lithammer/shortuuid v3.0.0+incompatible
+
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/lithammer/shortuuid v3.0.0+incompatible // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/shortener/handlers/shortener_handlers_test.go
@@ -0,0 +1,141 @@
+package handlers
+
+import (
+	"github.com/RomanAVolodin/go-url-shortener/internal/shortener/config"
+	"github.com/RomanAVolodin/go-url-shortener/internal/shortener/repositories"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestShortURLHandler(t *testing.T) {
+	type wanted struct {
+		code              int
+		exactResponse     string
+		responseStartWith string
+		locationHeader    string
+	}
+	tests := []struct {
+		name         string
+		requestURL   string
+		requestType  string
+		requestBody  string
+		repo         repositories.UrlsRepository
+		backRepo     repositories.UrlsRepository
+		wantedResult wanted
+	}{
+		{
+			name:        "Delete request should fail",
+			requestType: http.MethodDelete,
+			wantedResult: wanted{
+				code:          http.StatusBadRequest,
+				exactResponse: config.OnlyGetPostRequestAllowedError,
+			},
+		},
+		{
+			name:        "Put request should fail",
+			requestType: http.MethodPut,
+			wantedResult: wanted{
+				code:          http.StatusBadRequest,
+				exactResponse: config.OnlyGetPostRequestAllowedError,
+			},
+		},
+		{
+			name:        "Patch request should fail",
+			requestType: http.MethodPatch,
+			wantedResult: wanted{
+				code:          http.StatusBadRequest,
+				exactResponse: config.OnlyGetPostRequestAllowedError,
+			},
+		},
+		{
+			name:        "Url link should not be generated with empty body",
+			requestType: http.MethodPost,
+			wantedResult: wanted{
+				code:          http.StatusBadRequest,
+				exactResponse: config.RequestBodyEmptyError,
+			},
+		},
+		{
+			name:        "Url link should be generated",
+			requestType: http.MethodPost,
+			requestBody: "https://ya.ru",
+			repo:        make(repositories.UrlsRepository),
+			backRepo:    make(repositories.UrlsRepository),
+			wantedResult: wanted{
+				code:              http.StatusCreated,
+				responseStartWith: config.BaseURL,
+			},
+		},
+		{
+			name:        "Url link should be returned while creating if already generated",
+			requestType: http.MethodPost,
+			requestBody: "https://ya.ru",
+			repo:        make(repositories.UrlsRepository),
+			backRepo:    repositories.UrlsRepository{"https://ya.ru": "qwerty"},
+			wantedResult: wanted{
+				code:          http.StatusCreated,
+				exactResponse: config.BaseURL + "qwerty",
+			},
+		},
+		{
+			name:        "Url link should not be returned without id in query",
+			requestType: http.MethodGet,
+			wantedResult: wanted{
+				code:          http.StatusBadRequest,
+				exactResponse: config.NoIDWasFoundInURL,
+			},
+		},
+		{
+			name:        "Url link should be returned",
+			requestURL:  "/qwerty",
+			requestType: http.MethodGet,
+			requestBody: "https://ya.ru",
+			repo:        repositories.UrlsRepository{"qwerty": "https://ya.ru"},
+			backRepo:    make(repositories.UrlsRepository),
+			wantedResult: wanted{
+				code:           http.StatusTemporaryRedirect,
+				locationHeader: "https://ya.ru",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			url := "/"
+			if tt.requestURL != "" {
+				url = tt.requestURL
+			}
+			var request *http.Request
+			if tt.requestBody != "" {
+				request = httptest.NewRequest(tt.requestType, url, strings.NewReader(tt.requestBody))
+			} else {
+				request = httptest.NewRequest(tt.requestType, url, nil)
+			}
+			request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+
+			w := httptest.NewRecorder()
+			h := ShortenerHandler(tt.repo, tt.backRepo)
+			h.ServeHTTP(w, request)
+			res := w.Result()
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+
+			assert.Equal(t, tt.wantedResult.code, res.StatusCode)
+			assert.Nil(t, err)
+			if tt.wantedResult.exactResponse != "" {
+				assert.Equal(t, tt.wantedResult.exactResponse, strings.Trim(string(resBody), "\n"))
+			}
+			if tt.wantedResult.responseStartWith != "" {
+				assert.True(
+					t,
+					strings.HasPrefix(strings.Trim(string(resBody), "\n"), tt.wantedResult.responseStartWith),
+				)
+			}
+
+			assert.Equal(t, tt.wantedResult.locationHeader, res.Header.Get("Location"))
+		})
+	}
+}
