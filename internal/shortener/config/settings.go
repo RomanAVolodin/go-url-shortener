631907ac948f8e60b40612d6ce00884287d45d82
Roman Volodin <rozarioagro@gmail.com>
2022-12-31 18:33:57 +0300

    increment 7 semms to be done

--- internal/shortener/config/settings.go
+++ internal/shortener/config/settings.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"flag"
+	"fmt"
 	"github.com/caarlos0/env/v6"
 	"log"
+	"os"
 )
 
 const (
@@ -14,14 +17,24 @@ const (
 )
 
 type AppSettings struct {
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	ServerAddress   string `env:"SERVER_ADDRESS"`
+	BaseURL         string `env:"BASE_URL"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
 }
 
 var Settings AppSettings
 
 func init() {
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Использование консольной команды %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&Settings.ServerAddress, "a", "localhost:8080", "Server address with port")
+	flag.StringVar(&Settings.BaseURL, "b", "http://localhost:8080", "Full featured base url")
+	flag.StringVar(&Settings.FileStoragePath, "f", "", "File storage path")
+	flag.Parse()
+
 	err := env.Parse(&Settings)
 	if err != nil {
 		log.Fatal(err)
